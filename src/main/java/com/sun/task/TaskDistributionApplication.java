520ad8b4e356963f7f882cbdc609fe2292039a37
heping <61138194@qq.com>
2020-07-16 10:28:19 +0800

    第一次提交

--- src/main/java/com/sun/task/TaskDistributionApplication.java
+++ src/main/java/com/sun/task/TaskDistributionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskDistributionApplication {
 
     public static void main(String[] args) {
+        //测试
         SpringApplication.run(TaskDistributionApplication.class, args);
     }
 
